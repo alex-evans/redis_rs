16f6d1765905128f7a886df3d0df38a65ea2a5ea
Alex Evans <alex.evans.d@gmail.com>
2024-07-01 22:08:07 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
-// Uncomment this block to pass the first stage
 use std::net::TcpListener;
+use std::io::{Read, Write};
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = match TcpListener::bind("127.0.0.1:6379") {
+        Ok(listener) => listener,
+        Err(e) => {
+            println!("Failed to bind to port 6379: {}", e);
+            return;
+        }
+    };
     
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
-                println!("accepted new connection");
+                println!("New Connection Coming in");
+                let mut _processing_stream = _stream;
+                let mut buffer = [0; 1024];
+                _processing_stream.read(&mut buffer).unwrap();
+
+                // if buffer.starts_with(b"PING") {
+                _processing_stream.write_all(b"+PONG\r\n").unwrap();
+                _processing_stream.flush().unwrap();
+                println!("PONG!")
             }
             Err(e) => {
                 println!("error: {}", e);
