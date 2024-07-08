c77285ac9244f6d7f08c96575daaef2fd187b0c3
Alex Evans <alex.evans.d@gmail.com>
2024-07-07 21:34:11 -0500

    Update to handle multiple requests using tokio

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,40 @@
-use std::net::TcpListener;
-use std::io::{Read, Write};
-use std::str;
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Logs from your program will appear here!");
-
-    let listener = TcpListener::bind("127.0.0.1:6379").expect("Failed to bind to port 6379");
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
     
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                println!("Connection successful!");
-                let mut buffer = Vec::new();
-                let mut connection_buffer = [0; 1024];
-                
-                loop {
-                    match stream.read(&mut connection_buffer) {
-                        Ok(0) => {
-                            println!("Connection closed!");
-                            break;
-                        }
-                        Ok(bytes_read) => {
-                            buffer.extend_from_slice(&connection_buffer[..bytes_read]);
-                            let request = str::from_utf8(&buffer).unwrap();
-                            
-                            if request.contains("PING") {
-                                if let Err(e) = stream.write_all(b"+PONG\r\n") {
-                                    println!("Failed to write to connection: {}", e);
-                                    break;
-                                }
-                                if let Err(e) = stream.flush() {
-                                    println!("Failed to flush the connection: {}", e);
-                                    break;
-                                }
-                                println!("PONG sent!");
+    loop {
+        let (mut socker, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
+
+            loop {
+                match socker.read(&mut buf).await {
+                    Ok(0) => {
+                        println!("Connection closed");
+                        return;
+                    }
+                    Ok(_) => {
+                        let request = std::str::from_utf8(&buf).unwrap();
+                        if request.contains("PING") {
+                            if let Err(e) = socker.write_all(b"+PONG\r\n").await {
+                                println!("Failed to write to connection: {}", e);
+                                return;
                             }
-                            buffer.clear();   // Clear the buffer
+                            println!("PONG sent!");
                         }
-                        Err(e) => {
-                            println!("Failed to read from connection: {}", e);
-                            break;
-                        }
+                        buf.fill(0);  // Clear the buffer
+                    }
+                    Err(e) => {
+                        println!("Failed to read from connection: {}", e);
+                        return;
                     }
                 }
             }
-            Err(e) => {
-                println!("Failed to establish a connection: {}", e);
-            }
-        }
+        });
     }
 }
