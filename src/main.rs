c23dfe26c93ff9853bbae282a7d2565a77e0d236
Alex Evans <alex.evans.d@gmail.com>
2024-07-07 20:11:24 -0500

    Changed up how handling this

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::net::TcpListener;
 use std::io::{Read, Write};
+use std::str;
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -13,23 +14,36 @@ fn main() {
     };
     
     for stream in listener.incoming() {
+        println!("New connection!");
         match stream {
-            Ok(_stream) => {
-                let mut _processing_stream = _stream;
+            Ok(mut stream) => {
+                println!("Connection successful!");
                 let mut buffer = [0; 1024];
-                let buffer_alt = [0; 1024];
-                let buffer_str = String::from_utf8_lossy(&buffer_alt);
-                for line in buffer_str.lines() {
-                    if line.trim() == "PING" {
-                        _processing_stream.read(&mut buffer).unwrap();
-                        _processing_stream.write_all(b"+PONG\r\n").unwrap();
-                        _processing_stream.flush().unwrap();
-                        println!("PONG!")
+                
+                loop {
+                    match stream.read(&mut buffer) {
+                        Ok(0) => {
+                            println!("Connection closed!");
+                            break;
+                        }
+                        Ok(_) => {
+                            let request = str::from_utf8(&buffer).unwrap();
+                            if request.starts_with("PING") {
+                                stream.write_all(b"+PONG\r\n").unwrap();
+                                stream.flush().unwrap();
+                                println!("PONG!");
+                            }
+                            buffer.fill(0);   // Clear the buffer
+                        }
+                        Err(e) => {
+                            println!("Failed to read from connection: {}", e);
+                            break;
+                        }
                     }
                 }
             }
             Err(e) => {
-                println!("error: {}", e);
+                println!("Failed to establish a connection: {}", e);
             }
         }
     }
