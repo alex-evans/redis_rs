c6339302d68aae0c8ce05a8ddd075fe284ed9d87
Alex Evans <alex.evans.d@gmail.com>
2024-07-07 20:42:14 -0500

    New method

--- src/main.rs
+++ src/main.rs
@@ -5,35 +5,37 @@ use std::str;
 fn main() {
     println!("Logs from your program will appear here!");
 
-    let listener = match TcpListener::bind("127.0.0.1:6379") {
-        Ok(listener) => listener,
-        Err(e) => {
-            println!("Failed to bind to port 6379: {}", e);
-            return;
-        }
-    };
+    let listener = TcpListener::bind("127.0.0.1:6379").expect("Failed to bind to port 6379");
     
     for stream in listener.incoming() {
-        println!("New connection!");
         match stream {
             Ok(mut stream) => {
                 println!("Connection successful!");
-                let mut buffer = [0; 1024];
+                let mut buffer = Vec::new();
+                let mut connection_buffer = [0; 1024];
                 
                 loop {
-                    match stream.read(&mut buffer) {
+                    match stream.read(&mut connection_buffer) {
                         Ok(0) => {
                             println!("Connection closed!");
                             break;
                         }
-                        Ok(_) => {
+                        Ok(bytes_read) => {
+                            buffer.extend_from_slice(&connection_buffer[..bytes_read]);
                             let request = str::from_utf8(&buffer).unwrap();
-                            if request.starts_with("PING") {
-                                stream.write_all(b"+PONG\r\n").unwrap();
-                                stream.flush().unwrap();
-                                println!("PONG!");
+                            
+                            if request.contains("PING") {
+                                if let Err(e) = stream.write_all(b"+PONG\r\n") {
+                                    println!("Failed to write to connection: {}", e);
+                                    break;
+                                }
+                                if let Err(e) = stream.flush() {
+                                    println!("Failed to flush the connection: {}", e);
+                                    break;
+                                }
+                                println!("PONG sent!");
                             }
-                            buffer.fill(0);   // Clear the buffer
+                            buffer.clear();   // Clear the buffer
                         }
                         Err(e) => {
                             println!("Failed to read from connection: {}", e);
