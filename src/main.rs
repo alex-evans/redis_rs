773c42b9089a711232507b22a01342d729187cf0
Alex Evans <alex.evans.d@gmail.com>
2024-07-23 22:25:15 -0500

    Fix what echo line is referencing

--- src/main.rs
+++ src/main.rs
@@ -101,7 +101,7 @@ fn get_next_element(lines: &mut std::str::Lines) -> String {
 }
 
 fn build_echo_response(lines: &mut std::str::Lines) -> String {
-    let echo_line = lines.next().unwrap().to_string();
+    let echo_line = get_next_element(lines); // Skip the first line
     println!("Echo Line: {}", echo_line);
     let len_of_echo_line = echo_line.len();
     println!("Length of Echo Line: {}", len_of_echo_line);
