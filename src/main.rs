5fc7161282ebbe236874cd367c33dae25e247659
Alex Evans <alex.evans.d@gmail.com>
2024-07-23 22:11:14 -0500

    Fix slash backwardsness

--- src/main.rs
+++ src/main.rs
@@ -103,6 +103,6 @@ fn get_next_element(lines: &mut std::str::Lines) -> String {
 fn build_echo_response(lines: &mut std::str::Lines) -> String {
     let echo_line = lines.next().unwrap();
     let len_of_echo_line = echo_line.len();
-    let echo_response = format!("${}/r/n+{}/r/n", len_of_echo_line, echo_line);
+    let echo_response = format!("${}\r\n+{}\r\n", len_of_echo_line, echo_line);
     return echo_response;
 }
