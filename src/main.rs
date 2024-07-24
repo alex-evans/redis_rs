463b872790ab00a0953aff8151de7b5489dadbb7
Alex Evans <alex.evans.d@gmail.com>
2024-07-23 21:47:24 -0500

    Troubleshooting

--- src/main.rs
+++ src/main.rs
@@ -101,10 +101,9 @@ fn determine_number_of_elements(line: &str) -> i32 {
 
 fn get_next_element(lines: &mut std::str::Lines) -> String {
     println!("Getting next element");
-    println!("Lines 1: {:?}", lines);
     let _skip_line = lines.next().unwrap();
-    println!("Lines 2: {:?}", lines);
+    println!("_skip_line: {:?}", _skip_line.to_string());
     let return_line = lines.next().unwrap();
-    println!("Return Line: {:?}", return_line.to_string());
+    println!("return_line: {:?}", return_line.to_string());
     return return_line.to_string();
 }
