d9dfbc3fd61d0fbc1c8d67a226b01b6a01e24112
Alex Evans <alex.evans.d@gmail.com>
2024-07-26 21:25:42 -0500

    Fix bug - need to add 5 for role in length of info

--- src/handlers/info.rs
+++ src/handlers/info.rs
@@ -1,6 +1,6 @@
 
 pub fn handle_info_request() -> String {
     let role: String = "master".to_string();
-    let response = format!("${}\r\nrole:{}\r\n", role.len(), role);
+    let response = format!("${}\r\nrole:{}\r\n", role.len() + 5, role);
     return response;
 }
