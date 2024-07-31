fa5228b6bbce72b91256267313a4ec9d2711f771
Alex Evans <alex.evans.d@gmail.com>
2024-07-30 22:18:43 -0500

    Move call to master to its own helper function

--- src/command_types/replication.rs
+++ src/command_types/replication.rs
@@ -3,11 +3,7 @@ use std::sync::{Arc, Mutex};
 
 use crate::Config;
 use crate::SharedState;
-use std::net::TcpStream;
-use std::io::{Read, Write};
-
-// use crate::handlers::info::handle_info_request;
-
+use crate::helpers::helpers::send_message_to_server;
 
 pub fn store_replication(config_ref: &Config, ref_state: &Arc<Mutex<SharedState>>) -> () {
     println!("Storing Replication Config");
@@ -25,22 +21,8 @@ pub fn ping_master(ref_state: &Arc<Mutex<SharedState>>) -> () {
             let port = parts.get(1).unwrap_or(&"");
             let address = format!("{}:{}", host, port);
             println!("Ping - Connecting to Master: {}", address);
-
-            match TcpStream::connect(address) {
-                Ok(mut stream) => {
-                    let message = "*1\r\n$4\r\nPING\r\n";
-                    stream.write_all(message.as_bytes()).unwrap();
-                    
-                    let mut response = String::new();
-                    stream.read_to_string(&mut response).unwrap();
-                    
-                    println!("Ping response: {}", response);
-                }
-                Err(e) => {
-                    println!("Failed to connect to {}:{}", host, port);
-                    println!("Error: {}", e);
-                }
-            }
+            let message = "*1\r\n$4\r\nPING\r\n".to_string();
+            send_message_to_server(&address, message);
         }
         None => {
             println!("No Master to ping");
@@ -60,38 +42,14 @@ pub fn send_replication_data(ref_config: &Config, ref_state: &Arc<Mutex<SharedSt
             let master_port = parts.get(1).unwrap_or(&"");
             let address = format!("{}:{}", master_host, master_port);
             println!("Connecting to Master: {}", address);
+            
+            // First Message: Listening Port
+            let message = format!("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n${}\r\n{}\r\n", repl_port.len(), repl_port);
+            send_message_to_server(&address, message);
 
-            match TcpStream::connect(address) {
-                Ok(mut stream) => {
-                    let message = format!("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n${}\r\n{}\r\n", repl_port.len(), repl_port);
-                    stream.write_all(message.as_bytes()).unwrap();
-                    
-                    let mut response = String::new();
-                    stream.read_to_string(&mut response).unwrap();
-                    
-                    println!("Replication response: {}", response);
-                }
-                Err(e) => {
-                    println!("Failed to connect to {}:{}", master_host, master_port);
-                    println!("Error: {}", e);
-                }
-            }
-
-            // match TcpStream::connect(address) {
-            //     Ok(mut stream) => {
-            //         let message = "*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\npsync2\r\n".to_string();
-            //         stream.write_all(message.as_bytes()).unwrap();
-                    
-            //         let mut response = String::new();
-            //         stream.read_to_string(&mut response).unwrap();
-                    
-            //         println!("Replication response: {}", response);
-            //     }
-            //     Err(e) => {
-            //         println!("Failed to connect to {}:{}", master_host, master_port);
-            //         println!("Error: {}", e);
-            //     }
-            // }
+            // Second Message: Capabilities
+            let message = "*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\npsync2\r\n".to_string();
+            send_message_to_server(&address, message);
         }
         None => {
             println!("No Master to send replication data to");
--- src/helpers/helpers.rs
+++ src/helpers/helpers.rs
@@ -1,4 +1,6 @@
 
+use std::net::TcpStream;
+use std::io::{Read, Write};
 
 pub fn determine_number_of_elements(line: &str) -> i32 {
     let characters: String = line.chars().skip(1).collect();
@@ -16,4 +18,19 @@ pub fn get_next_element(lines: &mut std::str::Lines) -> String {
     let _skip_line = lines.next().unwrap_or("");
     let return_line = lines.next().unwrap_or("");
     return return_line.to_string();
+}
+
+pub fn send_message_to_server(address: &String, message: String) -> String {
+    let mut response = String::new();
+    match TcpStream::connect(address) {
+        Ok(mut stream) => {
+            stream.write_all(message.as_bytes()).unwrap();
+            stream.read_to_string(&mut response).unwrap();
+        }
+        Err(e) => {
+            println!("Failed to connect address");
+            println!("Error: {}", e);
+        }
+    }
+    return response;
 }
