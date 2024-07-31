1ef8bbb7eaddb1a04772a82e0300220f0abb316a
Alex Evans <alex.evans.d@gmail.com>
2024-07-30 20:46:02 -0500

    Troubleshooting

--- src/handlers/info.rs
+++ src/handlers/info.rs
@@ -4,6 +4,7 @@ use std::sync::{Arc, Mutex};
 use crate::SharedState;
 
 pub fn handle_info_request(state: &Arc<Mutex<SharedState>>) -> String {
+    println!("Handling INFO request");
     let state = state.lock().unwrap();
     let master_replid: String = "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb".to_string();
     let master_repl_offset: String = "0".to_string();
@@ -18,6 +19,8 @@ pub fn handle_info_request(state: &Arc<Mutex<SharedState>>) -> String {
         None => role = "master".to_string()
     }
 
+    println!("Role: {}", role);
+
     let response: String = format!(
         "${}\r\nrole:{}\r\nmaster_replid:{}\r\nmaster_repl_offset:{}\r\n",
         role.len() + 5 + master_replid.len() + 13 + master_repl_offset.len() + 18 + 6,  // Added 6 at the end for the \r\n bytes
@@ -25,5 +28,7 @@ pub fn handle_info_request(state: &Arc<Mutex<SharedState>>) -> String {
         master_replid,
         master_repl_offset
     );
+
+    println!("Response: {}", response);
     return response;
 }
