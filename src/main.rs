40a64f8d53bf2ace9504eb12dec138aa15ae10cf
Alex Evans <alex.evans.d@gmail.com>
2024-07-30 21:36:06 -0500

    Troubleshooting

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,9 @@ use redis_starter_rust::{
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Logs from your program will appear here!");
-
+    println!("Here we are");
     let config = Config::from_args(env::args().collect()).unwrap();
-
+    println!("And now we have a config");
     run(config).await
 }
 
