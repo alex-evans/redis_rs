5d153ff43d116358a9ed98fb306e355650a4c18c
Alex Evans <alex.evans.d@gmail.com>
2024-07-30 20:18:21 -0500

    Add masterreplid and masterreploffset

--- src/command_types/replication.rs
+++ src/command_types/replication.rs
@@ -4,11 +4,11 @@ use std::sync::{Arc, Mutex};
 use crate::Config;
 use crate::SharedState;
 
+use crate::handlers::info::handle_info_request;
 
-pub fn store_replication(config_ref: &Config, _request: &str, state: &Arc<Mutex<SharedState>>) -> String {
-    let mut state = state.lock().unwrap();
+
+pub fn store_replication(config_ref: &Config, _request: &str, ref_state: &Arc<Mutex<SharedState>>) -> String {
+    let mut state = ref_state.lock().unwrap();
     state.store.insert("replicaof".to_string(), config_ref.replicaof.clone());
-    let role: String = "slave".to_string();
-    let response = format!("${}\r\nrole:{}\r\n", role.len() + 5, role);
-    return response
+    return handle_info_request(&ref_state);
 }
--- src/handlers/info.rs
+++ src/handlers/info.rs
@@ -5,24 +5,25 @@ use crate::SharedState;
 
 pub fn handle_info_request(state: &Arc<Mutex<SharedState>>) -> String {
     let state = state.lock().unwrap();
+    let master_replid: String = "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb".to_string();
+    let master_repl_offset: String = "0".to_string();
+
+    let role: String;
     match state.store.get("replicaof") {
         Some(_full_value) => {
             // let parts: Vec<&str> = full_value.split("\r\n").collect();
             // let value = parts.get(0).unwrap_or(&"");
-            return_slave_info()
+            role = "slave".to_string()
         }
-        None => return return_master_info()
+        None => role = "master".to_string()
     }
-}
 
-fn return_slave_info() -> String {
-    let role: String = "slave".to_string();
-    let response = format!("${}\r\nrole:{}\r\n", role.len() + 5, role);
-    return response
-}
-    
-fn return_master_info() -> String {
-    let role: String = "master".to_string();
-    let response = format!("${}\r\nrole:{}\r\n", role.len() + 5, role);
+    let response: String = format!(
+        "${}\r\nrole:{}\r\nmaster_replid:{}\r\nmaster_repl_offset:{}\r\n",
+        role.len() + 5 + master_replid.len() + 15 + master_repl_offset.len() + 21,
+        role,
+        master_replid,
+        master_repl_offset
+    );
     return response;
 }
