62a17b51420c6551a990b22170b71b2aa35df980
Alex Evans <alex.evans.d@gmail.com>
2024-07-30 20:47:30 -0500

    Troubleshooting

--- src/handlers/info.rs
+++ src/handlers/info.rs
@@ -3,11 +3,15 @@ use std::sync::{Arc, Mutex};
 
 use crate::SharedState;
 
-pub fn handle_info_request(state: &Arc<Mutex<SharedState>>) -> String {
+pub fn handle_info_request(state_ref: &Arc<Mutex<SharedState>>) -> String {
     println!("Handling INFO request");
-    let state = state.lock().unwrap();
+    let state = state_ref.lock().unwrap();
+    println!("State Handled");
     let master_replid: String = "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb".to_string();
+    println!("Master Replid: {}", master_replid);
+
     let master_repl_offset: String = "0".to_string();
+    println!("Master Repl Offset: {}", master_repl_offset);
 
     let role: String;
     match state.store.get("replicaof") {
