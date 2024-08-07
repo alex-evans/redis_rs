b4cada333687f13e0b47ae349f8864af6dda181c
Alex Evans <alex.evans.d@gmail.com>
2024-08-06 20:40:08 -0500

    Troubleshooting

--- src/handlers/ping.rs
+++ src/handlers/ping.rs
@@ -6,5 +6,6 @@ use crate::helpers::helpers::send_message_to_server;
 pub async fn handle_ping_request(stream: &mut TcpStream) -> () {
     let ping_response = "+PONG\r\n".to_string();
     send_message_to_server(stream, &ping_response).await.unwrap();
+    println!("Sent PONG response to client");
     return
 }
