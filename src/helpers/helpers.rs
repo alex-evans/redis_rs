3e1736413745fc3b37b4f848ced5b0650e2f9616
Alex Evans <alex.evans.d@gmail.com>
2024-08-07 22:14:48 -0500

    Have response back from sending message to server

--- src/helpers/helpers.rs
+++ src/helpers/helpers.rs
@@ -1,5 +1,5 @@
 
-use tokio::io::AsyncWriteExt;
+use tokio::io::{AsyncWriteExt, AsyncBufReadExt, BufReader};
 use tokio::net::TcpStream;
 
 pub fn determine_number_of_elements(line: &str) -> i32 {
@@ -24,10 +24,9 @@ pub async fn send_message_to_server(stream: &mut TcpStream, message: &str) -> Re
     println!("Sending message to server: {}", message);
     AsyncWriteExt::write_all(stream, message.as_bytes()).await?;
 
-    // let mut reader = BufReader::new(stream);
-    // let mut response = String::new();
-    // AsyncBufReadExt::read_line(&mut reader, &mut response).await?;
-    // println!("Received response from server: {}", response);
-    // Ok(response)
-    Ok("".to_string())
+    let mut reader = BufReader::new(stream);
+    let mut response = String::new();
+    AsyncBufReadExt::read_line(&mut reader, &mut response).await?;
+    println!("Received response from server: {}", response);
+    Ok(response)
 }
