3ca225a6a4a560beab720f7cef3237ca17998dcd
Alex Evans <alex.evans.d@gmail.com>
2024-08-09 21:10:29 -0500

    troubelshooting

--- src/handlers/ping.rs
+++ src/handlers/ping.rs
@@ -5,7 +5,8 @@ use tokio::sync::Mutex;
 use crate::helpers::helpers::send_message_to_server;
 
 pub async fn handle_ping_request(stream: Arc<Mutex<TcpStream>>) -> () {
-    let ping_response = "+PONG\r\n".to_string();
+    let ping_response: String = "+PONG\r\n".to_string();
+    println!("Handling PING request");
     send_message_to_server(stream, &ping_response, false).await.unwrap();
     println!("Sent PONG response to client");
     return
