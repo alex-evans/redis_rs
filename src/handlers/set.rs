f25610b86a0d4bc42ed6d75f58e2f66ed975233c
Alex Evans <alex.evans.d@gmail.com>
2024-08-12 22:33:29 -0500

    Troubleshooting

--- src/handlers/set.rs
+++ src/handlers/set.rs
@@ -58,10 +58,10 @@ pub async fn handle_set_request<'a>(
             if let Err(e) = send_message_to_server(stream, &message, true).await {
                 eprintln!("Error sending message: {}", e);
             }
-            println!("Sending request: {}", request);
-            if let Err(e) = send_message_to_server(stream, &request, true).await {
-                eprintln!("Error sending request: {}", e);
-            }
+            println!("Would be Sending request: {}", request);
+            // if let Err(e) = send_message_to_server(stream, &request, true).await {
+            //     eprintln!("Error sending request: {}", e);
+            // }
             return
         }
     }
