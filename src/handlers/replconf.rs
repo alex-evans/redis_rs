6b15435413d2a2997201d89de8e2f3c7f6fc56a2
Alex Evans <alex.evans.d@gmail.com>
2024-08-16 20:51:57 -0500

    Troubleshooting

--- src/handlers/replconf.rs
+++ src/handlers/replconf.rs
@@ -16,13 +16,17 @@ pub async fn handle_replconf_request<'a>(
     // Store the wrapped stream in the shared state
     {
         let mut state_guard = state.lock().await;
+        println!("REPLCONF - Storing stream in shared state");
+        println!("REPLCONF - Stream: {:?}", stream);
         state_guard.stream = Some(stream.clone());
     }
 
     let message: String = "+OK\r\n".to_string();
     // Lock the stream to send a message
     {
+        println!("REPLCONF - Attempting to lock the stream...");
         let mut stream_lock = stream.lock().await;
+        println!("REPLCONF - Successfully locked the stream");
         send_message_to_server(&mut *stream_lock, &message, false).await.unwrap();
     }
 
