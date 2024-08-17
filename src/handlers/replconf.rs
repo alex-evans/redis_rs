69c5cfbd632e2ce9d99ddeed2c109f054940f381
Alex Evans <alex.evans.d@gmail.com>
2024-08-16 23:13:19 -0500

    Troubleshooting

--- src/handlers/replconf.rs
+++ src/handlers/replconf.rs
@@ -9,17 +9,17 @@ use crate::SharedState;
 pub async fn handle_replconf_request<'a>(
     stream: Arc<Mutex<TcpStream>>, 
     _lines: &'a mut std::str::Lines<'a>,
-    _state: &'a Arc<Mutex<SharedState>>
+    state: &'a Arc<Mutex<SharedState>>
 ) -> () {
     println!("Handling REPLCONF request");
     
-    // // Store the wrapped stream in the shared state
-    // {
-    //     let mut state_guard = state.lock().await;
-    //     println!("REPLCONF - Storing stream in shared state");
-    //     println!("REPLCONF - Stream: {:?}", stream);
-    //     state_guard.stream = Some(stream.clone());
-    // }
+    // Store the wrapped stream in the shared state
+    {
+        let mut state_guard = state.lock().await;
+        println!("REPLCONF - Storing stream in shared state");
+        println!("REPLCONF - Stream: {:?}", stream);
+        state_guard.stream = Some(stream.clone());
+    }
 
     let message: String = "+OK\r\n".to_string();
     // Lock the stream to send a message
